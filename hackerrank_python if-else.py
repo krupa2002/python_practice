e5026c8664bed9f7070f88a68cbfcd29c6a97434
krupa2002 <90334102+krupa2002@users.noreply.github.com>
2022-12-16 19:44:01 +0530

    Update hackerrank_python if-else.py

--- hackerrank_python if-else.py
+++ hackerrank_python if-else.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         print('Weird')
     elif n %2==0 and n>2 and n<5:
         print('Not Weird')  
-    elif n%2==0 and n >6 and n <20:
+    elif n%2==0 and n >6 and n <=20:
         print('Weird')
     else:
         print ('Not Weird')
