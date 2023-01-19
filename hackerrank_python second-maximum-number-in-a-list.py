0bed5c5f9c7d6bf2332b46007ec3361301e01b99
krupa2002 <90334102+krupa2002@users.noreply.github.com>
2023-01-19 12:08:52 +0530

    Create hackerrank_python second-maximum-number-in-a-list.py

---
+++ hackerrank_python second-maximum-number-in-a-list.py
@@ -0,0 +1,5 @@
+#Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    print(sorted(set(arr), reverse=True)[1])
